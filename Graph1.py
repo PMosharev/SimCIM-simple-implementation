a45a3e56b79f4f359190b64988d96088c3e1b9db
PMosharev <gfif.vji@yandex.ru>
2021-11-24 16:11:01 +0300

    Made Data and Out folders, add tex file out, add Cut Value count

--- Graph1.py
+++ Graph1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import random
 from GraphOperations import *
 
 
@@ -20,9 +21,9 @@ def DeltaXi(x, i, nu, Zita, J, f):
         Displacement += J[i][j]*x[j]
     return nu * x[i] + Zita * Displacement + f
 
-def Algor(x, n, Zita, Noize):
-    for t in range(n):
-        Nu = nu(t, n)
+def Algor(x, J, NSteps, Zita, Noize):
+    for t in range(NSteps):
+        Nu = nu(t, NSteps)
 
         for i in range(len(x)):
             f = Noize*np.random.normal()
@@ -30,36 +31,55 @@ def Algor(x, n, Zita, Noize):
             x[i] = phi(x[i], Deltaxi)
     return x
 
-J = [[0,1,1,0,0,0],[1,0,1,0,0,0],[1,1,0,1,0,0],[0,0,1,0,1,1],[0,0,0,1,0,1],[0,0,0,1,1,0]]
-#J = [[0,1,1,0,0],[1,0,1,1,0],[1,1,0,0,1],[0,1,0,0,1],[0,0,1,1,0]]
-#J = [[0,1],[1,0]]
-#J = [[0,2,2],[2,0,1],[2,1,0]]
-#J = [[0,1,1,1,1,1],[1,0,0,0,0,0],[1,0,0,0,0,0],[1,0,0,0,0,0],[1,0,0,0,0,0],[1,0,0,0,0,0]]
+def CutValue(x, J, n):
+    Cut = 0.0
+    for i in range(n):
+        for j in range(i):
+           Cut += J[i][j] * (1 - x[i] * x[j])
+    Cut = Cut * (- 0.5)
+    return Cut
 
-
-
-
-x = [0, 0, 0, 0, 0, 0]
-#x = [0, 0, 0, 0, 0]
-#x = [0, 0]
-#x = [0,0,0]
-#x = [0,0,0,0,0,0]
 
 Zita = - 0.05
-n = 200
+NSteps = 1000
 Noize = 0.00001
 
-xnew = Algor(x, n, Zita, Noize)
+print('Number of steps = ', NSteps, '\n')
 
-    
-print(xnew)
 
-Jnew = ModifyMatrix(J,xnew)
+NumberOfFiles = 3
+FileNames = []
+##
+##for i in range(NumberOfFiles):
+##    FileNames.append('./Data/SimpleGraph' + str(i+1) + '.txt')
 
-FileOutName = '1.png'
-MatrixG = nx.DiGraph(np.matrix(J))
-ImageGraph(MatrixG, SetColors(x), FileOutName)
+FileNames.append('./Data/G39.txt')
+FileNames.append('./Data/G1.txt')
+FileNames.append('./Data/G22.txt')
 
-FileOutName = '2.png'
-MatrixGnew = nx.DiGraph(np.matrix(Jnew))
-ImageGraph(MatrixGnew, SetColors(x), FileOutName)
+for FileName in FileNames:
+    J = AdjMatrixFromFile(FileName)
+
+    with open(FileName) as fil:
+        n = int(fil.readline().split()[0])
+    x = [0 for j in range(n)]
+    x = Algor(x, J, NSteps, Zita, Noize)
+    RandX = [random.choice([-1, 1]) for j in range(n)]
+    Jnew = ModifyMatrix(J,x)
+    Cut = CutValue(x, J, n)
+    RandomCut = CutValue(RandX, J, n)
+    print('Source file: ', FileName)
+    #print(x)
+    print('Cut Value =', Cut)
+    print('Cut Value for random cut = ', RandomCut)
+    print()
+##    SourceImageName = './Out/Source' + str(i+1) + '.png'
+##    ResultImageName = './Out/Result' + str(i+1) + '.png'
+##    ImageGraph(nx.DiGraph(np.matrix(J)), SetColors(x), SourceImageName)
+##    ImageGraph(nx.DiGraph(np.matrix(Jnew)), SetColors(x), ResultImageName)
+        
+
+OutTexFileName = './Out/OutTexFile.tex'
+
+#CreateTexFile(Zita, NSteps, Noize, NumberOfFiles, FileNames, OutTexFileName)
+
--- GraphOperations.py
+++ GraphOperations.py
@@ -3,10 +3,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import copy
 
-def ImageGraph(G, Colors, FileName):
-    nx.draw(G, node_color = Colors, with_labels=True)
-    plt.savefig(FileName)
-    plt.clf()
 
 def GraphFromFile(FileName):
     g = nx.Graph()
@@ -18,23 +14,31 @@ def GraphFromFile(FileName):
             g.add_edge(vert1, vert2)
     return g
 
+def AdjMatrixFromFile(FileName):
+    with open(FileName) as fil:
+        n, m = [int(j) for j in fil.readline().split()]
+        J = [[0 for i in range(n)] for j in range(n)]
+        #PrintMatrix(J)
+        for k in range(m):
+            vert1, vert2, weight = [int(j) for j in fil.readline().split()]
+            J[vert1 - 1][vert2 - 1] = weight
+            J[vert2 - 1][vert1 - 1] = weight
+    return J
+
+
+def ImageGraph(G, Colors, FileName):
+    nx.draw(G, node_color = Colors, with_labels=True)
+    plt.savefig(FileName)
+    plt.clf()
+
+
+
 def PrintMatrix(Matrix):
     for i in Matrix:
         for j in i:
             print(j, end = '')
         print()
 
-
-def AdjMatrixFromFile(FileName):
-    with open(FileName) as fil:
-        n, m = [int(j) for j in fil.readline().split()]
-        J = [[0 for i in range(n)] for j in range(n)]
-        PrintMatrix(J)
-        for k in range(m):
-            vert1, vert2, weight = [int(j) for j in fil.readline().split()]
-            J[vert1 - 1][vert2 - 1] = weight
-            J[vert2 - 1][vert1 - 1] = weight
-    return J
 
 def ModifyMatrix(J, x):
     Jnew = copy.deepcopy(J)
@@ -57,42 +61,28 @@ def SetColors(x):
     return Colors
 
 
-##FileName = 'SimpleGraph1.txt'
-##x = [1, -1]
-##
-##J = AdjMatrixFromFile(FileName)
-##
-##
-##Jnew = ModifyMatrix(J, x)
-##
-##PrintMatrix(J)
-##print('')
-##PrintMatrix(Jnew)
-##
-##FileOutName = '1.png'
-##MatrixG = nx.DiGraph(np.matrix(J))
-##ImageGraph(MatrixG, FileOutName)
-##
-##FileOutName = '2.png'
-##MatrixGnew = nx.DiGraph(np.matrix(Jnew))
-##ImageGraph(MatrixGnew, FileOutName)
+def CreateTexFile(Zita, NSteps, Noize, NumberOfFiles, FileNames, OutTexFileName):
+    with open(OutTexFileName, 'w') as OutTex:
 
+        with open('TexTemplate.txt') as tmp:
+            for s in tmp:
+                OutTex.write(s)
+        OutTex.write('Parameters: ' + r'\\' + '\n')
+        OutTex.write('$\zeta = ' + str(Zita) + '$, ' + r'\\' + ' \n')
+        OutTex.write('Number of steps =  $' + str(NSteps) + '$, ' + r'\\' + ' \n')
+        OutTex.write('Noize Parameter = $ ' + str(Noize) + '$, ' + r'\\' + ' \n')
 
-##for i in range(5):
-##    FileName = 'SimpleGraph' + str(i+1) + '.txt'
-##    FileOutName = 'GraphImage' + str(i+1) + '.png'
-##    #PrintGraph(GraphFromFile(FileName), FileOutName)
-##    J = AdjMatrixFromFile(FileName)
-##    PrintMatrix(J)
-##    MatrixG = nx.DiGraph(np.matrix(J))
-##    ImageGraph(MatrixG, FileOutName)
-##    print('')
-    
+        for i in range(NumberOfFiles):
+            OutTex.write(r'\newpage' + '\n')
+            OutTex.write('FileName: ' + FileNames[i] + r'\\' + '\n')
+            OutTex.write('Source Graph: '+ r'\\' + '\n')
+            OutTex.write(r'\f' + 'box{\includegraphics[scale = 0.8]{Source' + str(i+1) +'.png}} \n \n')
+            OutTex.write('Result Graph: '+ r'\\' + '\n')
+            OutTex.write(r'\f' + 'box{\includegraphics[scale = 0.8]{Result' + str(i+1) +'.png}} \n \n')
         
-##FileName = 'G1.txt'
-##FileOutName = 'G1plot.png'
-##PrintGraph(GraphFromFile(FileName), FileOutName)
-  
-#J = [[0,1,1,0,0,0],[1,0,1,0,0,0],[1,1,0,1,0,0],[0,0,1,0,1,1],[0,0,0,1,0,1],[0,0,0,1,1,0]]
+
+        OutTex.write('\end{document}')
+
+    return 0
 
 
