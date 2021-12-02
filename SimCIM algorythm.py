5b5327da0aa975b6b7dbe54bf80a9bb1f0562ee8
PMosharev <gfif.vji@yandex.ru>
2021-12-02 23:00:42 +0300

    Switched to work with nx graph and sparse matrix

--- SimCIM algorythm.py
+++ SimCIM algorythm.py
@@ -9,73 +9,38 @@ import copy
 
 # implementation of SimCIM algorythm from article https://arxiv.org/pdf/1901.08927.pdf (by Tiunov et. al)
 
-class MyGraph:
-    def __init__(self, WeightedVertices = False):
-
-        self.J = np.zeros(0)        
-        
-        self.V = np.zeros(0)
-        self.x = np.zeros(0)
-            
-    def Upload(self, FileName, WeightedVertices = False):
-        with open(FileName) as fil:
-            n, m = [int(j) for j in fil.readline().split()]
-
-            self.n = n
-            self.m = m
 
             
-            self.x = np.zeros(n)
-            J = np.zeros(shape = (n,n), dtype = int)
+def UploadGraph(FileName, WeightedVertices = False):
+    with open(FileName) as fil:
+        n, m = [int(j) for j in fil.readline().split()]
 
-            for i in range(m):
-                vert1, vert2, weight = [int(j) for j in fil.readline().split()]
-                J[vert1 - 1][vert2 - 1] = weight
-                J[vert2 - 1][vert1 - 1] = weight
+        G = nx.Graph()
 
-            self.J = J
+        for i in range(n):
+            G.add_node(i)
+            
+        for i in range(m):
+            vert1, vert2, weight = [int(j) for j in fil.readline().split()]
+            G.add_edge(vert1 - 1,vert2 - 1, weight = weight)
+            
 
-            if WeightedVertices:
-                for i in range(n):
-                    _, self.V[i] = [int(j) for j in fil.readline().split()]
-            else:
-                self.V = np.ones(n)
+        if WeightedVertices:
+            for i in range(n):
+                _, G.nodes[i]['weight'] = [int(j) for j in fil.readline().split()]
+
+    return n, G
+
+                
 
                     
-    def Draw(self, FileName, ):
-        Colors = []
-        for i in self.x:
-            if i == -1:
-                Colors.append('red')
-            elif i == 1:
-                Colors.append('green')
-            else:
-                Colors.append('blue')
 
-        SourceImageName = './Images/' + FileName[7:-4] + '_Original' + '.png'
-        GSource = nx.Graph(np.matrix(self.J))
-        plt.figure(figsize = (self.n // 3 + 5, self.n // 3 + 5))
-        nx.draw(GSource, pos = nx.circular_layout(GSource), with_labels=True)
-        plt.savefig(SourceImageName)
-        plt.clf()              
-        
-
-        
-        ResultImageName = './Images/' + FileName[7:-4] + '_Result' + '.png'
-        
-        GResult = nx.Graph(np.matrix(Jnew))
-        nx.draw(GResult, pos = nx.spring_layout(GResult), node_color = Colors, with_labels=True)
-        plt.savefig(ResultImageName)
-        plt.clf()
-
-        nx.write_gexf(GSource, './Results/' + FileName[7:-4] + '_Original' + '.gexf')
-        nx.write_gexf(GSource, './Results/' + FileName[7:-4] + '_Result' + '.gexf')
                     
 
 
 
 
-def SimCIM(J, x, n, NSteps = 1000, Nu0 = -0.5, Zeta = -0.05, Noize = 0.005):
+def SimCIM(J,n, NSteps = 1000, Nu0 = -0.5, Zeta = -0.05, Noize = 0.005):
 
     print('Number of steps = ', NSteps)
     print('Zeta = ', Zeta)
@@ -92,13 +57,15 @@ def SimCIM(J, x, n, NSteps = 1000, Nu0 = -0.5, Zeta = -0.05, Noize = 0.005):
     Res.write('\n')
     Res.close()
 
+    x = np.zeros(n)
+
     for t in range(NSteps):
 
         Nu = Nu0*(1 - math.tanh(t / NSteps * 6 - 3))     # pump-loss factor, Fig2 (b) in the article
         fgen = (np.random.normal() for i in range(n))
         f = np.fromiter(fgen, float) * Noize
         
-        x += Nu * x +  Zeta * x.dot(J) + f
+        x += Nu * x +  Zeta * np.dot(x, J.A) + f
 
         for i in range(n):     # can I get rid of such direct elementwise checking?
             if x[i] > 1:
@@ -115,11 +82,16 @@ def SimCIM(J, x, n, NSteps = 1000, Nu0 = -0.5, Zeta = -0.05, Noize = 0.005):
     return x
 
     
-def MaxCut(G):
-     return SimCIM(G.J, G.x, G.n)
+def MaxCut(G, n):    
+    J = nx.adjacency_matrix(G)    
+    return SimCIM(J, n)
 
-def MinCut(G, Ab = 1):
-    return SimCIM(-G.J + G.V * Ab , G.x, G.n)
+def MinCut(G, n, Ab = 3):
+    J = nx.adjacency_matrix(G)
+    V = np.array([i[1] for i in G.nodes(data = "weight")])
+    
+    print(V)
+    return SimCIM(- J.T + V * Ab , n)
     
 
 
@@ -149,46 +121,40 @@ FileNames = []
 
 
 
-FileNames.append('./Data/SyntheticGraph1.txt')
+#FileNames.append('./Data/SyntheticGraph1.txt')
 #FileNames.append('./Data/SyntheticGraph2.txt')
 #FileNames.append('./Data/SyntheticGraph3.txt')
+
+FileNames.append('./Data/SimpleWeightedGraph.txt')
 
 
 for FileName in FileNames:
 
-    Gr = MyGraph()
-    Gr.Upload(FileName)
+    
+    n, G = UploadGraph(FileName, True)
 
 
 
     print('Source file: ', FileName)
-    print('Number of vertices = ', Gr.n)
+    print('Number of vertices = ', n)
     print('Work started')
     Res.write('Source file: ' + FileName + '\n')
-    Res.write('Number of vertices: ' + str(Gr.n) + '\n')
+    Res.write('Number of vertices: ' + str(n) + '\n')
     
 
 
     time3 = time.time()
     
-    x = MinCut(Gr)
+    x = MinCut(G,n)
 
 
-
-    Jnew = copy.deepcopy(Gr.J)
+    Cut = 0.0
+    NewGraph = G.copy()
     for i in range(len(x)):
         for j in range(i):
-            if x[i] != x[j]:
-                Jnew[i][j] = 0
-                Jnew[j][i] = 0
-    
-    
-    Cut = 0.0
-    for i in range(Gr.n):
-        for j in range(i):
-           Cut += Gr.J[i][j] * (1 - x[i] * x[j])
-    Cut = Cut * 0.5
-
+            if x[i] != x[j] and (i, j) in NewGraph.edges:                
+                NewGraph.remove_edge(j, i)
+                Cut += 1
 
     PlusClusterPower = 0
     for i in x:
@@ -203,8 +169,8 @@ for FileName in FileNames:
     
     print('Cut Value =', Cut)
     
-    print('Balance value (0.5 is perfect) ', round(PlusClusterPower/Gr.n, 2))
-    Res.write('Balance value (0.5 is perfect) ' + str(round(PlusClusterPower/Gr.n, 2)) + '\n')
+    print('Balance value (0.5 is perfect) ', round(PlusClusterPower/n, 2))
+    Res.write('Balance value (0.5 is perfect) ' + str(round(PlusClusterPower/n, 2)) + '\n')
     
     print('Time spent: ', round(time4 - time3, 6), ' seconds \n')
 
@@ -215,9 +181,34 @@ for FileName in FileNames:
     Res.write('\n \n')
     
 
+    if n < 201: # Draw images for small graph
+        
+        Colors = []
+        for i in x:
+            if i == -1:
+                Colors.append('red')
+            elif i == 1:
+                Colors.append('green')
+            else:
+                Colors.append('blue')
 
-    if Gr.n < 201: # Draw images for small graph
-        Gr.Draw(FileName)
+        SourceImageName = './Images/' + FileName[7:-4] + '_Original' + '.png'
+        
+        plt.figure(figsize = (n // 3 + 5, n // 3 + 5))
+        nx.draw(G, pos = nx.circular_layout(G), with_labels=True)
+        plt.savefig(SourceImageName)
+        plt.clf()              
+        
+
+        
+        ResultImageName = './Images/' + FileName[7:-4] + '_Result' + '.png'
+
+        nx.draw(NewGraph, pos = nx.spring_layout(NewGraph), node_color = Colors, with_labels=True)
+        plt.savefig(ResultImageName)
+        plt.clf()
+
+        nx.write_gexf(G, './Results/' + FileName[7:-4] + '_Original' + '.gexf')
+        nx.write_gexf(NewGraph, './Results/' + FileName[7:-4] + '_Result' + '.gexf')
         
         
         
