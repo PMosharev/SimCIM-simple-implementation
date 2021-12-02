d6f91de7f6c03e8910f505e1528c44ce1490762b
PMosharev <gfif.vji@yandex.ru>
2021-12-02 17:51:22 +0300

    allocated graph upload into a seprate module

--- SimCIM algorythm.py
+++ SimCIM algorythm.py
@@ -9,6 +9,35 @@ import copy
 
 # implementation of SimCIM algorythm from article https://arxiv.org/pdf/1901.08927.pdf (by Tiunov et. al)
 
+class MyGraph:
+    def __init__(self):
+
+        self.J = np.zeros(shape = (0)) 
+        
+        self.V = np.zeros(shape = (0))
+            
+    def Upload(self, FileName, Weighted = False):
+        with open(FileName) as fil:
+            n, m = [int(j) for j in fil.readline().split()]
+
+            self.n = n
+            self.m = m
+            
+            J = np.zeros(shape = (n,n), dtype = int)
+
+            for i in range(m):
+                vert1, vert2, weight = [int(j) for j in fil.readline().split()]
+                J[vert1 - 1][vert2 - 1] = weight
+                J[vert2 - 1][vert1 - 1] = weight
+
+            self.J = J
+
+            if Weighted:
+                for i in range(n):
+                    _, V[i] = [int(j) for j in fil.readline().split()]
+                    
+
+
 
 
 Zeta = - 0.05
@@ -57,24 +86,19 @@ FileNames = []
 
 
 FileNames.append('./Data/SyntheticGraph1.txt')
-FileNames.append('./Data/SyntheticGraph2.txt')
-FileNames.append('./Data/SyntheticGraph3.txt')
+#FileNames.append('./Data/SyntheticGraph2.txt')
+#FileNames.append('./Data/SyntheticGraph3.txt')
 
 
 for FileName in FileNames:
+
+    Gr = MyGraph()
+    Gr.Upload(FileName)
     
-    with open(FileName) as fil:
-        n, m = [int(j) for j in fil.readline().split()]
-        
-        J = np.zeros(shape = (n,n), dtype = int)
 
-        for k in range(m):
-            vert1, vert2, weight = [int(j) for j in fil.readline().split()]
-            J[vert1 - 1][vert2 - 1] = weight
-            J[vert2 - 1][vert1 - 1] = weight
-
-
-
+    J = Gr.J
+    n = Gr.n
+    m = Gr.m
                 
     x = np.zeros(n)
 
@@ -138,13 +162,12 @@ for FileName in FileNames:
            Cut += J[i][j] * (1 - x[i] * x[j])
     Cut = Cut * 0.5
 
-    MinusClusterPower = 0
+
     PlusClusterPower = 0
     for i in x:
         if i == 1:
             PlusClusterPower += 1
-        elif i == -1:
-            MinusClusterPower += 1
+        
     
    
 
@@ -152,12 +175,10 @@ for FileName in FileNames:
 
     
     print('Cut Value =', Cut)
-    if MinusClusterPower != 0:
-        print('Balance value (1 is perfect) ', round(PlusClusterPower/MinusClusterPower, 2))
-        Res.write('Balance value (1 is perfect) ' + str(round(PlusClusterPower/MinusClusterPower, 2)) + '\n')
-    else:
-        print('Balance value (1 is perfect) ', round(PlusClusterPower, 2))
-        Res.write('Balance value (1 is perfect) ' + str(round(PlusClusterPower, 2)) + '\n')
+    
+    print('Balance value (0.5 is perfect) ', round(PlusClusterPower/n, 2))
+    Res.write('Balance value (0.5 is perfect) ' + str(round(PlusClusterPower/n, 2)) + '\n')
+    
     print('Time spent: ', round(time4 - time3, 6), ' seconds \n')
 
 
